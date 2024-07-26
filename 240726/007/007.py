a58a66fc023acf426d91fd2cca7c7553edfc8676
paplpap <97944596+paplpap@users.noreply.github.com>
2024-07-26 10:02:32 +0900

    Title: 007, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240726/007/007.py
@@ -0,0 +1,17 @@
+a,b,c = input().split()
+
+
+class Student:
+    def __init__(self, aa, bb, cc):
+        self.aa = aa
+        self.bb = bb
+        self.cc = cc
+
+    
+
+
+
+Student = Student(a,b,c)
+print('secret code :',Student.aa)
+print('meeting point :',Student.bb)
+print('time :',Student.cc)
