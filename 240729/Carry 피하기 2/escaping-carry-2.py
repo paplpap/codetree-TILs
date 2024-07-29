5bd5af9e7a102cc02e0e0fe2c725f8cf6f631999
paplpap <97944596+paplpap@users.noreply.github.com>
2024-07-29 16:35:48 +0900

    Title: Carry 피하기 2, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/Carry 피하기 2/escaping-carry-2.py
+++ 240729/Carry 피하기 2/escaping-carry-2.py
@@ -9,25 +9,30 @@ carry = []
 #자릿수 별로 더하기
 def lenght(a,b,c):
     flag = True
-    for _ in range(n):
+    
+    for _ in range(len(str(max(a,b,c)))):
         a,a_ = divmod(a,10)
         b,b_ = divmod(b,10)
         c,c_ = divmod(c,10)
 
-        if a_ + b_ + c_ > 10:
+        if (a_ + b_ + c_) >= 10:
             flag = False
             break
-    
+
     return flag
 
 
-for i in range(n):
-    for j in range(i,n-1):
-        for k in range(j,n-2):
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
             a = arr[i]
             b = arr[j]
             c = arr[k]
             if lenght(a,b,c):
                 carry.append(a+b+c)
 
-print(max(carry))
+
+if carry == []:
+    print(-1)
+else:
+    print(max(carry))
