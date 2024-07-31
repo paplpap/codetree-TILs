4a8d2a6e53af4f16ddff6ebc1aea4669c25705ca
paplpap <97944596+paplpap@users.noreply.github.com>
2024-07-31 20:27:02 +0900

    Title: 배열 채우기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/배열 채우기/filling-array.py
+++ 240731/배열 채우기/filling-array.py
@@ -3,8 +3,11 @@ arr=list(map(int,input().split()))
 a=''
 for i in arr[::-1]:
     if i == 0:
-        break
+        a = ''
+        continue
+        
     else:
-        a += str(i)
-        print(a,end=' ')
-    a = ''
+        i = str(i)
+        a += ' '+i
+
+print(a[1:])
