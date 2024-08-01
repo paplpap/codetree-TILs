93bb176a8de2a7fdc0db2e4ef525cdc3cc434657
paplpap <97944596+paplpap@users.noreply.github.com>
2024-08-01 13:52:11 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240801/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,10 @@
+n,k = map(int,input().split())
+
+list_ = [0]*7
+
+for i in range(k):
+    a,b = map(int,input().split())
+    for j in range(a-1,b):
+        list_[j] += 1
+    
+print(max(list_))
