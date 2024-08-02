8774fe27b359ce30b7937248565ddb3b65165ce3
paplpap <97944596+paplpap@users.noreply.github.com>
2024-08-02 16:50:58 +0900

    Title: 1까지 나누기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/1까지 나누기/divide-by-1.py
@@ -0,0 +1,24 @@
+#n = int(input())
+
+#cnt = 0
+#while n > 1:
+#    cnt += 1
+#    n = n // cnt
+    
+
+#print(cnt)
+
+n = int(input())
+
+cnt = 0
+div = n
+for i in range(1, n+1):
+    div = div//i
+    cnt+=1
+    
+    if div>=1:
+        continue
+    else:
+        break
+
+print(cnt)
