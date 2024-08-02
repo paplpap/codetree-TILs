7d140082c37c1b4147584222af8d8b7988756260
paplpap <97944596+paplpap@users.noreply.github.com>
2024-08-02 13:49:22 +0900

    Title: 최대로 겹치는 구간, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240802/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -3,7 +3,7 @@ li = [0]*201
 n = int(input())
 for i in range(n):
     a,b = map(int,input().split())
-    for j in range(a,b+1):
+    for j in range(a,b):
         li[100+j] += 1
 
-print(max(li)-1)
+print(max(li))
