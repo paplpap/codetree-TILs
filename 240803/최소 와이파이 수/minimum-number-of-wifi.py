1295ffb3e97a4f0c74e46986d0b2980b6770983a
paplpap <97944596+paplpap@users.noreply.github.com>
2024-08-03 23:16:38 +0900

    Title: 최소 와이파이 수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240803/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,12 +2,20 @@ n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 
 cnt = 0
-for i in range(m,n-m+1,2*m+1):
-    for j in range(i-m,i+m+1):
-    
-        if arr[j] == 1:
-            cnt += 1
-            
-            break
 
-print(cnt)
+if m == 0:
+    for i in range(n):
+
+        if arr[i] == 1:
+            
+            cnt += 1
+    print(cnt)    
+else:
+    for i in range(m,n-m+1,2*m+1):
+        for j in range(i-m,i+m+1):
+        
+            if arr[j] == 1:
+                cnt += 1
+                break
+
+    print(cnt)
