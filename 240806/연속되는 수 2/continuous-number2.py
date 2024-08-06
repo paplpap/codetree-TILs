ee8f08267db1f9f54a7dfaff5d201ccb49f54f26
paplpap <97944596+paplpap@users.noreply.github.com>
2024-08-06 18:17:02 +0900

    Title: 연속되는 수 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,18 @@
+n= int(input())
+
+now = -1
+maxc = 1
+cnt = 1
+for i in range(n):
+    
+    a = int(input())
+    
+    if now != a:
+        now = a
+        maxc = max(cnt,maxc)
+        cnt = 1
+    else:
+        cnt += 1
+    
+
+print(maxc)
